966e2d0039828a0caff74dc4c5882bf422c45db5
shkr <shashank.f1@gmail.com>
2025-02-11 20:42:17 +0530

    reorganize

--- src/agentcompany/application/strategy/mongodb.py
+++ src/agentcompany/application/strategy/mongodb.py
@@ -1,8 +1,8 @@
 import os
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb://mongo:nGsLscIRmxgkGxFfKDdlKdopkrCzipis@junction.proxy.rlwy.net:45148" # os.getenv("MONGO_URI", "mongodb://localhost:27017/")
-MONGO_SYSTEM_URI = "mongodb://mongo:yBOctqkWozxRqVgNBNqXupbDiSkNOhFB@junction.proxy.rlwy.net:30525" # os.getenv("MONGO_SYSTEM_URI")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+MONGO_SYSTEM_URI = os.getenv("MONGO_SYSTEM_URI")
 
 def mongodb_connect() -> MongoClient:
     client = MongoClient(MONGO_URI)
