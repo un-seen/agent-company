2773271e910c2995ddc1d832cfd43dd7e85f56a6
shkr <shashank.f1@gmail.com>
2025-04-21 10:35:09 +0530

    fix function agent

--- src/agentcompany/extensions/agents/function.py
+++ src/agentcompany/extensions/agents/function.py
@@ -12,15 +12,24 @@ def FunctionAgent(name: str,
                  session_id: str,
                  interface_id: str, 
                  description: str, 
-                 custom_yaml_path: str = None) -> FunctionPattern:
+                 mod_yaml_path: str = None,
+                 custom_yaml_path: str = None,
+                 mcp_servers: List[ModelContextProtocolImpl] = [],
+                 model_id="gpt-4o-mini") -> FunctionPattern:
     """
     Create a Function agent.
     """
     default_yaml_path = importlib.resources.files("agentcompany.extensions.prompts.function").joinpath("default.yaml")
     default_prompt_templates: Dict = yaml.safe_load(default_yaml_path.read_text())
-    # agent_yaml_path = importlib.resources.files("agentcompany.extensions.prompts.residual").joinpath("python.yaml")
-    # updated_prompt_templates: Dict = yaml.safe_load(agent_yaml_path.read_text())
     updated_prompt_templates = default_prompt_templates
+    # Mod
+    if mod_yaml_path:
+        mod_yaml_path = importlib.resources.files("agentcompany.extensions.prompts.flow").joinpath(f"{mod_yaml_path}.yaml")
+        mod_yaml_path = Path(mod_yaml_path)
+        file_reader =  open(mod_yaml_path, 'r')
+        mod_prompt_templates: Dict = yaml.safe_load(file_reader)
+        updated_prompt_templates = merge_dicts(updated_prompt_templates, mod_prompt_templates)
+    # Custom
     if custom_yaml_path:
         custom_yaml_path = Path(custom_yaml_path)
         file_reader =  open(custom_yaml_path, 'r')
@@ -33,6 +42,6 @@ def FunctionAgent(name: str,
         session_id=session_id,
         interface_id=interface_id, 
         description=description, 
-        model=OpenAIServerLLM("gpt-4o-mini"),
+        model=OpenAIServerLLM(model_id=model_id),
         prompt_templates=prompt_templates,
     )
