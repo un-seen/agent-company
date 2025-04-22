b515d96e46189976d8bf5d281da7a9057ecba1b4
shkr <shashank.f1@gmail.com>
2025-04-22 16:07:48 +0530

    remove monitoring

--- src/agentcompany/llms/monitoring.py
+++ src/agentcompany/llms/monitoring.py
@@ -1,51 +1,10 @@
 import json
 from enum import IntEnum
-from typing import List, Optional
 from datetime import datetime, timezone
 from redis import Redis
 import os
 from rich.console import Console
 from rich.markdown import Markdown
-
-
-class Monitor:
-    def __init__(self, tracked_model, logger):
-        self.step_durations = []
-        self.tracked_model = tracked_model
-        self.logger = logger
-        if getattr(self.tracked_model, "last_input_token_count", "Not found") != "Not found":
-            self.total_input_token_count = 0
-            self.total_output_token_count = 0
-
-    def get_total_token_counts(self):
-        return {
-            "input": self.total_input_token_count,
-            "output": self.total_output_token_count,
-        }
-
-    def reset(self):
-        self.step_durations = []
-        self.total_input_token_count = 0
-        self.total_output_token_count = 0
-
-    def update_metrics(self, step_log):
-        """Update the metrics of the monitor.
-
-        Args:
-            step_log ([`MemoryStep`]): Step log to update the monitor with.
-        """
-        step_duration = step_log.duration
-        self.step_durations.append(step_duration)
-        console_outputs = f"[Step {len(self.step_durations) - 1}: Duration {step_duration:.2f} seconds"
-
-        if getattr(self.tracked_model, "last_input_token_count", None) is not None:
-            self.total_input_token_count += self.tracked_model.last_input_token_count
-            self.total_output_token_count += self.tracked_model.last_output_token_count
-            console_outputs += (
-                f"| Input tokens: {self.total_input_token_count:,} | Output tokens: {self.total_output_token_count:,}"
-            )
-        console_outputs += "]"
-        self.logger.log(console_outputs, level=LogLevel.INFO)
 
 
 class LogLevel(IntEnum):
@@ -99,7 +58,7 @@ class AgentLogger:
         if isinstance(level, str):
             level = LogLevel[level.upper()]
         if level <= self.level:
-            # self.console.print(*args, **kwargs)
+            # Print to redis
             if hasattr(self, "redis_client"):
                 data_dict = {}
                 data_dict["role"] = self.name
